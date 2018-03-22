b93e61391ab011f29188d494ea83c018238f05e1
yprocks <yppatel181093@gmail.com>
2018-03-22 04:44:41 -0400

    Added Teleportation
    Added Portal Particle System
    Added Portal Script

--- Com313-GameProject/Assets/_Scripts/BarbarianScripts/GameManager.cs
+++ Com313-GameProject/Assets/_Scripts/BarbarianScripts/GameManager.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
 
 namespace _Scripts.BarbarianScripts
 {
@@ -11,22 +13,39 @@ namespace _Scripts.BarbarianScripts
         public GameObject[] Enemy;
         public int TotalEnemies = 20;
         public Text LevelText;
+        public GameObject Portal;
+
+        public float PortalSpawnTime;
+
+        // Portal Transform Variables
+        private GameObject _cutScenesCamera;
+
+        private GameObject[] _portals;
+
+        private bool _enteredPortal = false;
+        private GameObject _destPortal;
+
+        private Vector3 _targetPortalPosition;
+
+        private GameObject _playerObjectCollided;
+
+        private Coroutine _teleportCoroutine;
+        // Portal Transform Variables
 
         [SerializeField] private GameObject _player;
         private int _enemyCount;
         private int _currentLevel;
-        private float _generatedSpawnTime = 1;
+        private readonly float _generatedSpawnTime = 1;
         private float _currentSpawnTime = 0;
+
+        private GameObject[] _portalsSpawns;
+        private bool _spawnedPortals = false;
+        private float _portalSpawnTimer;
 
         public int CurrentLevel
         {
             get { return _currentLevel; }
             private set { _currentLevel = value; }
-        }
-        
-        public GameManager()
-        {
-            GameOver = false;
         }
 
         public bool GameOver { get; private set; }
@@ -48,9 +67,12 @@ namespace _Scripts.BarbarianScripts
 
         public void Start()
         {
-            _currentLevel = 1;
+            _currentLevel = SceneManager.GetActiveScene().buildIndex;
             _enemyCount = 0;
             LevelText.text = "Level " + _currentLevel;
+            _cutScenesCamera = GameObject.Find("Follow Camera");
+            GameOver = false;
+            _portalSpawnTimer = PortalSpawnTime;
             InvokeRepeating("SpawnEnemies", 0f, 1f);
         }
 
@@ -75,6 +97,38 @@ namespace _Scripts.BarbarianScripts
             {
                 _currentSpawnTime = 0;
             }
+
+            if (CurrentLevel <= 1) return;
+
+            if (CurrentLevel == 2)
+                if (_enteredPortal)
+                    MoveCameraToTargetPortal();
+
+            if (_portalsSpawns == null)
+                _portalsSpawns = GameObject.FindGameObjectsWithTag("PortalSpawn");
+            else if (!_spawnedPortals)
+            {
+                _portalSpawnTimer -= Time.deltaTime;
+
+                if (_portalSpawnTimer <= 0)
+                    SpawnPortals();
+            }
+        }
+
+        private void SpawnPortals()
+        {
+            var i = 0;
+            while (i < 2)
+            {
+                var spawn = _portalsSpawns[Random.Range(0, _portalsSpawns.Length)];
+                if (spawn.transform.childCount != 0) continue;
+                var obj = Instantiate(Portal, spawn.transform.position, Quaternion.identity);
+                obj.transform.parent = spawn.transform;
+                i++;
+            }
+
+            _spawnedPortals = true;
+            _portalSpawnTimer = PortalSpawnTime;
         }
 
         public void PlayerHit(int currentHp)
@@ -82,9 +136,59 @@ namespace _Scripts.BarbarianScripts
             GameOver = currentHp <= 0;
         }
 
+        public void EnterPortal(GameObject portalCollided, GameObject playerCollided)
+        {
+            _portals = GameObject.FindGameObjectsWithTag("PortalSpawn");
+            _portals = GameObject.FindGameObjectsWithTag("Portal");
+            _destPortal = portalCollided;
+            while (_destPortal == portalCollided) _destPortal = _portals[Random.Range(0, _portals.Length)];
+            _targetPortalPosition = _destPortal.transform.parent.position;
+            _cutScenesCamera.GetComponent<CameraFollow>().enabled = false;
+            _playerObjectCollided = playerCollided;
+            _playerObjectCollided.SetActive(false);
+            _teleportCoroutine = StartCoroutine(TriggerTeleportation(true));
+        }
+
+        private IEnumerator TriggerTeleportation(bool value)
+        {
+            yield return new WaitForSeconds(1f);
+            _enteredPortal = value;
+            if (_enteredPortal) yield break;
+            _playerObjectCollided.transform.position = _targetPortalPosition + new Vector3(2, 0, 2);
+            _playerObjectCollided.SetActive(true);
+            _cutScenesCamera.GetComponent<CameraFollow>().enabled = true;
+            if (_spawnedPortals) DestroyPortals();
+            _portalSpawnTimer = PortalSpawnTime;
+            _spawnedPortals = false;
+        }
+
+        private void DestroyPortals()
+        {
+            foreach (var portal in _portals)
+                Destroy(portal);
+        }
+
+        private void MoveCameraToTargetPortal()
+        {
+            var position = Vector3.Lerp(_cutScenesCamera.transform.position,
+                _targetPortalPosition,
+                Time.deltaTime * 2);
+            position.y = _cutScenesCamera.transform.position.y;
+            _cutScenesCamera.transform.position = position;
+
+            var point1 = new Vector2(_cutScenesCamera.transform.position.x, _cutScenesCamera.transform.position.z);
+            var point2 = new Vector2(_targetPortalPosition.x, _targetPortalPosition.z);
+
+            if (Vector2.Distance(point1, point2) > 3.0f)
+                return;
+
+            _teleportCoroutine = StartCoroutine(TriggerTeleportation(false));
+//            StopCoroutine(_teleportCoroutine);
+        }
+
         private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
         {
-            this._currentLevel = SceneManager.GetActiveScene().buildIndex + 1;
+            this._currentLevel = SceneManager.GetActiveScene().buildIndex;
             LevelText.text = "Level " + _currentLevel;
             if (scene.name == "GameOver")
             {
---
+++ Com313-GameProject/Assets/_Scripts/BarbarianScripts/PortalScript.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+using Vuforia;
+
+namespace _Scripts.BarbarianScripts
+{
+    public class PortalScript : MonoBehaviour
+    {
+//        private GameObject _cutScenesCamera;
+//
+//        private GameObject _player;
+//        private GameObject[] _portals;
+//
+//        private bool _enteredPortal = false;
+//        private GameObject _destPortal;
+//        private Vector3 _targetPosition;
+//
+        private SphereCollider _collider;
+
+        private void Start()
+        {
+            _collider = GetComponent<SphereCollider>();
+            Invoke("EnableColliders", 1f);
+        }
+
+        private void EnableColliders()
+        {
+            _collider.enabled = true;
+        }
+        
+//        private void Update()
+//        {
+//            if (_enteredPortal)
+//                MoveCameraToTargetPortal();
+//        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!other.CompareTag("Player")) return;
+            GameManager.Instance.EnterPortal(this.gameObject, other.gameObject);
+//            _portals = GameObject.FindGameObjectsWithTag("Portal");
+//            _destPortal = this.gameObject;
+//            while (_destPortal == this.gameObject)
+//                _destPortal = _portals[Random.Range(0, _portals.Length)];
+//            _targetPosition = _destPortal.transform.parent.position;
+//            _cutScenesCamera.GetComponent<CameraFollow>().enabled = false;
+//            _player = other.gameObject;
+//            _player.SetActive(false);
+//            StartCoroutine(EnterPortal(true));
+        }
+
+//        private IEnumerator EnterPortal(bool value)
+//        {
+//            yield return new WaitForSeconds(1f);
+//            _enteredPortal = value;
+//            if (!_enteredPortal)
+//            {
+//                _player.transform.position = _targetPosition + new Vector3(2, 0, 2);
+//                _player.SetActive(true);
+//                _cutScenesCamera.GetComponent<CameraFollow>().enabled = true;
+//            }
+//
+//            yield return null;
+//        }
+//
+//        private void MoveCameraToTargetPortal()
+//        {
+//            var position = Vector3.Lerp(_cutScenesCamera.transform.position,
+//                _targetPosition,
+//                Time.deltaTime * 2);
+//            position.y = _cutScenesCamera.transform.position.y;
+//            _cutScenesCamera.transform.position = position;
+//
+//            var point1 = new Vector2(_cutScenesCamera.transform.position.x, _cutScenesCamera.transform.position.z);
+//            var point2 = new Vector2(_targetPosition.x, _targetPosition.z);
+//
+//            if (Vector2.Distance(point1, point2) > 3.0f)
+//                return;
+//
+//            StartCoroutine(EnterPortal(false));
+//        }
+        
+        
+    }
+}
